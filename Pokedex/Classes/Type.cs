0d275c8884a16a66fd30f58fbf9b5721c7f3bcfd
Your Name <you@example.com>
2022-10-13 11:19:48 +0200

    attributs + constructeur + methodes add/remove type

--- Pokedex/Classes/Type.cs
+++ Pokedex/Classes/Type.cs
@@ -6,7 +6,63 @@ using System.Threading.Tasks;
 
 namespace Pokedex.Classes
 {
-    internal class Type
+    class Type
     {
+        public string Nom { get; set; }
+        List<Type> AvantageAttaque { get; set; }
+        List<Type> AvantageDefense { get; set; }
+        List<Type> FaiblesseAttaque { get; set; }
+        List<Type> FaiblesseDefense { get; set; }
+
+        public Type() 
+        {
+            Nom = "";
+            AvantageAttaque = new List<Type>();
+            AvantageDefense = new List<Type>();
+            FaiblesseAttaque = new List<Type>();
+            FaiblesseDefense = new List<Type>();
+
+        }
+        public Type(string nom)
+        {
+            Nom = nom;
+            AvantageAttaque = new List<Type>();
+            AvantageDefense = new List<Type>();
+            FaiblesseAttaque = new List<Type>();
+            FaiblesseDefense = new List<Type>();
+        }
+        public void AddAvantageAttaque(Type type) 
+        {
+            AvantageAttaque.Add(type);
+        }
+        public void AddAvantageDefense(Type type)
+        {
+            AvantageDefense.Add(type);
+        }
+        public void AddFaiblesseAttaque(Type type)
+        {
+            FaiblesseAttaque.Add(type);
+        }
+        public void AddFaiblesseDefense(Type type)
+        {
+            FaiblesseDefense.Add(type);
+        }
+        public void RemoveAvantageAttaque(Type type)
+        {
+            AvantageAttaque.Remove(type);
+        }
+        public void RemoveAvantageDefense(Type type)
+        {
+            AvantageDefense.Remove(type);
+        }
+        public void RemoveFaiblesseAttaque(Type type)
+        {
+            FaiblesseAttaque.Remove(type);
+        }
+        public void RemoveFaiblesseDefense(Type type)
+        {
+            FaiblesseDefense.Remove(type);
+        }
+
     }
 }
