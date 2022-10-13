bc176dae0dca65e4eb17d7ba83e0e1619272cdff
Sebast2711 <oillosebast@gmail.com>
2022-10-13 11:09:31 +0200

    Ajout attributs et constructeur caracteristique

--- Pokedex/Classes/Caracteristique.cs
+++ Pokedex/Classes/Caracteristique.cs
@@ -8,6 +8,30 @@ namespace Pokedex.Classes
 {
     class Caracteristique
     {
+        public string statistique { get; set; }
+        public byte valeur { get; set; }
+        public byte IV { get; set; }
+        public byte EV { get; set; }
+
+        public Caracteristique()
+        {
+            this.statistique = "";
+            this.valeur = 0;
+            this.IV = 0;
+            this.EV = 0;
+        }
+        public Caracteristique(string statistique, byte valeur, byte IV, byte EV)
+        {
+            this.statistique = statistique;
+            this.valeur = valeur;
+            this.IV = IV;
+            this.EV = EV;
+        }
+        public Caracteristique(string statistique, byte valeur)
+        {
+            this.statistique = statistique;
+            this.valeur = valeur;
+        }
 
     }
 }
