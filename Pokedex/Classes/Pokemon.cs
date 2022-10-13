2726598ff6a434de942615b21d47435abc5c823f
Sebast2711 <oillosebast@gmail.com>
2022-10-13 10:41:07 +0200

    classes

---
+++ Pokedex/Classes/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedex.Classes
+{
+    class Pokemon
+    {
+        public string Nom { get; set; }
+        public string Image { get; set; }
+        public Boolean vivant { get; set; }
+        public string Description { get; set; }
+
+
+
+    }
+}
