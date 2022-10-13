603932520ad4ca7e72ff32ee1f3ccb2576fbc1c6
Your Name <you@example.com>
2022-10-13 11:35:53 +0200

    attributs + constructeur pokemon

--- Pokedex/Classes/Pokemon.cs
+++ Pokedex/Classes/Pokemon.cs
@@ -1,19 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pokedex.Classes
 {
     class Pokemon
     {
         public string Nom { get; set; }
+        public Type Type { get; set; }
         public string Image { get; set; }
+        public List<Attaque> Attaques { get; set; }
+        public List<Caracteristique> Caracteristiques { get; set; }
         public Boolean vivant { get; set; }
+        public Statuts statuts { get; set; }
         public string Description { get; set; }
-
-
+        
+        public Pokemon () 
+        {
+            this.Nom = "";
+            this.Type = new Type();
+            this.Image = "";
+            this.Attaques = new List<Attaque>();
+            this.Caracteristiques = new List<Caracteristique>();
+            this.vivant = true;
+            this.statuts = new Statuts();
+            this.Description = "";
+        }
+        public Pokemon (string nom, Type type, string image,string description) 
+        {
+            this.Nom = nom;
+            this.Type = type;
+            this.Image = image;
+            this.Attaques = new List<Attaque> ();
+            this.Caracteristiques = new List<Caracteristique> ();
+            this.vivant = true;
+            this.statuts = new Statuts ();
+            this.Description = description;
+        }
 
     }
 }
