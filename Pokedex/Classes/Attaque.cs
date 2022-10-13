6a0ba94de3966de80bf400abb72020d9730c1b09
Sebast2711 <oillosebast@gmail.com>
2022-10-13 11:21:30 +0200

    ajout attributs et constructeurs Attaques

--- Pokedex/Classes/Attaque.cs
+++ Pokedex/Classes/Attaque.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace Pokedex.Classes
 {
-    internal class Attaque
+    class Attaque
     {
+        public string Nom { get; set; }
+        public byte Degats { get; set; }
+        public Caracteristique Caracteristique { get; set; }
+        public Statuts Statuts { get; set; }
+        public byte ChanceStatut { get; set; }
+        public byte ChanceUtilisatiom { get; set; }
+        public byte NombreUtilisation { get; set; }
+        public Type TypeAttaque { get; set; }
+
+
+        public Attaque()
+        {
+            this.Nom = "";
+            this.Degats = 0;
+            this.Caracteristique = new Caracteristique();
+            this.Statuts = new Statuts();
+            this.ChanceStatut = 0;
+            this.ChanceUtilisatiom = 0;
+            this.NombreUtilisation = 0;
+            this.TypeAttaque = new Type();
+        }
+
+        public Attaque(string nom, byte degats, Caracteristique caracteristique, Statuts statuts, byte chanceStatut, byte chanceUtilisatiom, byte nombreUtilisation, Type typeAttaque)
+        {
+            this.Nom = nom;
+            this.Degats = degats;
+            this.Caracteristique = caracteristique;
+            this.Statuts = statuts;
+            this.ChanceStatut = chanceStatut;
+            this.ChanceUtilisatiom = chanceUtilisatiom;
+            this.NombreUtilisation = nombreUtilisation;
+            this.TypeAttaque = typeAttaque;
+        }
+
+        public Attaque(string nom, byte degats)
+        {
+            this.Nom = nom;
+            this.Degats = degats;
+            this.Caracteristique = new Caracteristique();
+            this.Statuts = new Statuts();
+            this.ChanceStatut = 0;
+            this.ChanceUtilisatiom = 0;
+            this.NombreUtilisation = 0;
+            this.TypeAttaque = new Type();
+        }
+
+
+
     }
 }
