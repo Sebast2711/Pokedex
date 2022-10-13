73f8b31cb9f4a42bec73d74fa56b88fdae2d40eb
Your Name <you@example.com>
2022-10-13 11:03:14 +0200

    attributs + constructeur statuts

--- Pokedex/Classes/Statuts.cs
+++ Pokedex/Classes/Statuts.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Pokedex.Classes
 {
-    internal class Statuts
+    class Statuts
     {
+        public string Nom { get; set; }
+        public byte DegatsInfliges { get; set; }
+        public byte NombreTour { get; set; }
+        public byte ChanceStatus { get; set; }
+
+        public Statuts() 
+        {
+            this.Nom = "";
+            this.DegatsInfliges = 0;
+            this.NombreTour = 0;
+            this.ChanceStatus = 0;
+
+        }
+        public Statuts(string nom, byte degatsInfliges, byte nombreTour, byte chanceStatus)
+        {
+            this.Nom = nom;
+            this.DegatsInfliges = degatsInfliges;
+            this.NombreTour = nombreTour;
+            this.ChanceStatus = chanceStatus;
+        }
     }
 }
